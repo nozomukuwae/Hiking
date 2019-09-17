eb8f13219b8428527be6a18ab9b748420a6c0edf
Nozomu Kuwae <nozomukuwae@gmail.com>
2019-09-17 14:13:43 +0900

    Initial Commit

---
+++ Hiking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hiking
+//
+//  Created by 桑江 望 on 2019/09/17.
+//  Copyright © 2019 桑江 望. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
